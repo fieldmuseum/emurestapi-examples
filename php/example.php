00b486112dd43e4e9ebced4c683e5a2f9dfb06b8
Pete Herbst <peteherbst@me.com>
2024-12-14 12:33:57 -0600

    Remove monolog

--- php/example.php
+++ php/example.php
@@ -6,6 +6,5 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__, '.env');
 $dotenv->load();
 
 $authToken = getAuthToken($_ENV['EMUAPI_USER'], $_ENV['EMUAPI_PASSWORD']);
-$log->debug('Auth token', [$authToken]);
 
 print_r($authToken);
