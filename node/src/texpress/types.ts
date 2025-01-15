48eb477a88c451096fbbeb47653a219080cc0b90
Pete Herbst <peteherbst@me.com>
2025-01-15 11:49:31 -0600

    Implement node search

--- node/src/texpress/types.ts
+++ node/src/texpress/types.ts
@@ -6,6 +6,7 @@ export interface EMuRecord {
   data: RecordData
 }
 
+// Customize this to match your data
 interface RecordData {
   irn: {
     id: string
@@ -13,4 +14,32 @@ interface RecordData {
   }
   RightsAcknowledgeLocal?: string
   MulDescription?: string
+}
+
+// interface for Texpress Search
+// https://help.emu.axiell.com/emurestapi/3.1.3/04-Resources-Texpress.html
+export interface SearchResults {
+  hits: number
+  matches: SearchMatch[]
+}
+
+interface SearchMatch {
+  id: string
+  version: number
+  data: SearchMatchData
+}
+
+// Customize this to match your data
+interface SearchMatchData {
+  irn: {
+    id: string
+    '@controls': object
+  }
+  NamFullName?: string
+  NamBriefName?: string
+}
+
+export interface FormData {
+  key: string
+  value: string
 }
